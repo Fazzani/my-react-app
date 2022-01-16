1083400db7cb052f1e79de83ae88a1db980300cc
Heni FAZZANI <heni.fazzani.ext@up.coop>
2022-01-16 19:58:22 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { UpDataGrid, UpBox } from '@up-group-ui/react-controls';
+
+const data: any[] = [];
+for (var i = 0; i < 50; i++) {
+  data.push({
+    c1: 'Value ' + i,
+    c2: false,
+    c3: 'Value 3',
+    c4: { Libelle: 'Suivi', Couleur: '#369' },
+  });
+}
 
 function App() {
   return (
@@ -10,15 +20,40 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <UpBox></UpBox>
+      {/* <UpDataGrid
+        isPaginationEnabled={false}
+        isSelectionEnabled={false}
+        isSortEnabled={false}
+        columns={[
+          {
+            label: 'Col 1',
+            field: 'c1',
+            isSortable: true,
+          },
+          {
+            label: 'Col 2',
+            field: 'c2',
+            type: 'boolean',
+            isSortable: true,
+          },
+          {
+            label: 'Col 3',
+            field: 'c3',
+            isSortable: true,
+          },
+          {
+            label: 'Col 4',
+            field: 'c4',
+            isSortable: true,
+          },
+        ]}
+        data={data}
+      /> */}
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
     </div>
   );
 }
